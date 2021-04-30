2522754721b36e66bf6bfd90cdfc337ab9a086ff
Shinichi Sekine <ssshsysn@gmail.com>
2021-04-30 15:29:23 +0900

    Board マス目作成処理をハードコーディングから 2 つのループに変更

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ function Square(props) {
 class Board extends React.Component {
   renderSquare(i) {
     return (
-      <Square 
+      <Square
+        key={i} 
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -21,23 +22,20 @@ class Board extends React.Component {
   }
 
   render() {
+    const rows = [0, 1, 2];
+    const cols = [0, 1, 2];
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows.map(row => {
+          return (
+            <div
+              className="board-row"
+              key={row}
+            >
+              {cols.map(col => this.renderSquare(row * 3 + col))}
+            </div>
+          );
+        })}
       </div>
     );
   }
